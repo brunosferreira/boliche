7f0d7d50b7a042eac847536e768af73e6448b6c7
Bruno da Silva Ferreira <bs.ferreiras@gmail.com>
2021-06-18 09:33:35 -0400

    ajustando o git ignore do projeto para nao commitar arquivos desnecessarios

---
+++ CalculadoraDeBoliche/JogadorPartida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculadoraDeBoliche
+{
+    public class JogadorPartida
+    {
+        public int Id { get; private set; }
+        public Jogador Jogador { get; private set; }
+        public Partida Partida { get; private set; }
+        public List<Jogada> Jogadas { get; private set; }
+        public int PontuacaoTotal => Jogadas.Sum(j => j.Pontuacao);
+
+        public JogadorPartida(int id, Jogador jogador, Partida partida)
+        {
+            Id = id;
+            Jogador = jogador;
+            Partida = partida;
+            Jogadas = new List<Jogada>();
+        }
+
+        public void AdicionaJogada(Jogada jogada)
+        {
+            ValidaJogadaAdicionada(jogada);
+            Jogadas.Add(jogada);
+        }
+
+        private void ValidaJogadaAdicionada(Jogada jogada)
+        {
+            foreach(var _jogada in Jogadas)
+            {
+                if (jogada.Equals(_jogada))
+                    throw new ArgumentException("Não deve ser adicionada a mesma jogada mais de uma vez para o jogador.");
+            }
+        }
+    }
+}
---
+++ CalculadoraDeBolicheTeste/JogadorPartidaTest.cs
@@ -0,0 +1,44 @@
+﻿using CalculadoraDeBoliche;
+using CalculadoraDeBolicheTeste._Builders;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using ExpectedObjects;
+
+namespace CalculadoraDeBolicheTeste
+{
+    public class JogadorPartidaTest
+    {
+        [Fact]
+        public void DeveCriarUmJogadorPartida()
+        {
+            var jogador = JogadorBuilder.Novo().Criar();
+            var partida = PartidaBuilder.Novo().Criar();
+            
+            var jogadorPartidaEsperado = new
+            {
+                Id = 1,
+                Jogador = jogador,
+                Partida = partida,
+                Jogadas = new List<Jogada>()
+            };
+
+            var jogadorPartida = new JogadorPartida(jogadorPartidaEsperado.Id, jogadorPartidaEsperado.Jogador, jogadorPartidaEsperado.Partida);
+
+            jogadorPartidaEsperado.ToExpectedObject().ShouldMatch(jogadorPartida);
+        }
+
+        [Fact]
+        public void NaoDeveAdicionarDuasVezesAMesmaJogadaNaPartida()
+        {
+            var jogador = JogadorBuilder.Novo().Criar();
+            var partida = PartidaBuilder.Novo().Criar();
+            var jogadorPartida = new JogadorPartida(1, jogador, partida);
+            var jogada = JogadaBuilder.Novo().Criar();
+
+            jogadorPartida.AdicionaJogada(jogada);
+
+            Assert.Throws<ArgumentException>(() => jogadorPartida.AdicionaJogada(jogada));
+        }
+    }
+}
